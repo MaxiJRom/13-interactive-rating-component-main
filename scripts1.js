e31d152422f0742487cb2a591575ae6a4d114359
MaxiJRom <maxiromero11@hotmail.com>
2023-09-02 18:04:55 +0200

    wtf

--- scripts1.js
+++ scripts1.js
@@ -1,44 +1,44 @@
-window.addEventListener("DOMContentLoaded", (event) => {
-  // Grab HTML Elements
+/* window.addEventListener("DOMContentLoaded", (event) => { */
+// Grab HTML Elements
 
-  const submitButton = document.querySelector(".submit-button");
+const submitButton = document.querySelector(".submit-button");
 
-  const anchor = document.getElementById("anchor");
+const anchor = document.getElementById("anchor");
 
-  const puntaje = [];
+const puntaje = [];
 
-  let puntajeElegido;
-  let ultimaSeleccion;
-  let haySeleccion = false;
+let puntajeElegido;
+let ultimaSeleccion;
+let haySeleccion = false;
 
-  for (let i = 0; i <= 4; i++) {
-    puntaje[i] = document.querySelector(`.list-item:nth-child(${i + 1}`);
+for (let i = 0; i <= 4; i++) {
+  puntaje[i] = document.querySelector(`.list-item:nth-child(${i + 1}`);
 
-    puntaje[i].addEventListener("click", (e) => {
-      e.preventDefault();
+  puntaje[i].addEventListener("click", (e) => {
+    e.preventDefault();
 
-      puntajeElegido = e.target.innerHTML;
+    puntajeElegido = e.target.innerHTML;
 
-      if (haySeleccion) {
-        ultimaSeleccion.classList.remove("list-item--active");
-        ultimaSeleccion = e.target;
-      } else {
-        ultimaSeleccion = e.target;
-        haySeleccion = true;
-      }
-
-      e.target.classList.add("list-item--active");
-    });
-  }
-
-  submitButton.addEventListener("click", (e) => {
     if (haySeleccion) {
-      localStorage.puntajeElegido = puntajeElegido;
-
-      anchor.href = "./thank-you.html";
+      ultimaSeleccion.classList.remove("list-item--active");
+      ultimaSeleccion = e.target;
     } else {
-      e.target.prevenDefault();
-      anchor.href = "./index.html";
+      ultimaSeleccion = e.target;
+      haySeleccion = true;
     }
+
+    e.target.classList.add("list-item--active");
   });
+}
+
+submitButton.addEventListener("click", (e) => {
+  if (haySeleccion) {
+    localStorage.puntajeElegido = puntajeElegido;
+
+    anchor.href = "./thank-you.html";
+  } else {
+    e.target.prevenDefault();
+    anchor.href = "./index.html";
+  }
 });
+/* }) */
